6737005ff83ee91531a484f226ef2e80531c8615
MemeWasTaken <memelazzari02@gmail.com>
2021-12-16 15:51:09 +0100

    static imgs

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const app = new Vue ({
+    el: '#containerVue',
+    data: {
+        counter: 0,
+        imgs: [
+            'image1.jpg',
+            'image2.jpg',
+            'image3.jpg',
+            'image4.jpg'
+        ],
+    },
+    methods: {
+
+    }
+});
